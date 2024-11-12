8db53a4e3ff2b5e4a18b943bda932efae9785e56
Guilherme Medeiros <guilhermedeamorimmedeiros@yahoo.com.br>
2024-11-12 09:56:36 -0300

    Inicio do projeto
    - Inicio do projeto
    - Exibir os 20 primeiros resultados da API;
      - Permitir ordenação por nome do personagem;
      - Permitir filtrar por nome, pelo campo de busca;

--- src/services/marvelApi.ts
+++ src/services/marvelApi.ts
@@ -2,8 +2,8 @@ import axios, { AxiosInstance } from 'axios';
 import { md5 } from 'js-md5';
 
 // Substitua pelas suas chaves
-const PUBLIC_KEY = '63484ecf52f4fdba3424ad7677ed4582';
-const PRIVATE_KEY = 'a2ecf57067bff919e612e311268389107b2f4dcc';
+const PUBLIC_KEY = '63484ecf52f4fdba3424ad7677ed4582'; //chave publica
+const PRIVATE_KEY = 'a2ecf57067bff919e612e311268389107b2f4dcc'; // chave privada
 const timestamp = new Date().getTime();
 const hash = md5(timestamp + PRIVATE_KEY + PUBLIC_KEY);
 
